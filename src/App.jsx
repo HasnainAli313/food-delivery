6fc38fd44364a892294d733a9908c4e740b86c1f
Hasnain Ali <93475112+HasnainAli313@users.noreply.github.com>
2024-09-03 09:31:17 +0500

    added Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
   return (
+    <>
     <Navbar></Navbar>
+    <Hero></Hero>
+    </>
   )
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+
+function Hero() {
+  return (
+    <section className='hero flex  flex-col place-content-center'>
+        <div className="text-center">
+        <input className=' w-[50%]  p-1' type="text" placeholder='Search for pizza' />
+        <button className='m-2 p-1  rounded-md bg-red-600 hover:bg-red-800 font-semibold text-white w-[90px] border border-white '>Search</button>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
